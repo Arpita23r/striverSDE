c9ff286b039ef227e7e9b0913b2d6ccfc72ae72d
Arpita Roy <100989922+Arpita23r@users.noreply.github.com>
2024-08-06 21:03:10 +0530

    Create InversionOfArray.java

---
+++ InversionOfArray.java
@@ -0,0 +1,18 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+    public static long getInversions(long arr[], int n) {
+        // Write your code here.
+        long count=0;
+        int i=0;
+        while(i<n-1) {
+            for(int j=i+1;j<n;j++) {
+                if(arr[i]>arr[j])
+                count++;
+            }
+        
+        i++;
+    }
+    return count;
+    }
+}
